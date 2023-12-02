9de44fe1f706447014c8cd5987db8f00973f74e1
Vincenzo Racca <vincenzo.racca22@gmail.com>
2023-12-02 15:21:45 +0100

    update spring-doc

--- spring-resource-server/src/main/java/com/vincenzoracca/springresourceserver/security/SecurityConfig.java
+++ spring-resource-server/src/main/java/com/vincenzoracca/springresourceserver/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vincenzoracca.springresourceserver.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,16 +10,14 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     String[] whiteList = {
-            "/v3/api-docs**",
-            "/configuration/ui",
-            "/swagger-resources/**",
-            "/configuration/security",
+            "/v3/api-docs/**",
             "/swagger-ui.html",
-            "/webjars/**"
+            "/swagger-ui/**"
     };
 
     @Bean
